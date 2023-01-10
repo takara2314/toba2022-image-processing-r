976ba2b5ead93559b1554de6f8547e5b7805ab32
takara2314 <takara2314_TK@outlook.jp>
2023-01-10 09:59:11 +0900

    Add: 家具を抽出できるように

--- kabegami_system/suggest.py
+++ kabegami_system/suggest.py
@@ -1,7 +1,31 @@
+import cv2
 from kabegami_system.load_img import load_img
 
 # 壁紙の色を提案する
 def suggest(img_path):
     img = load_img(img_path)
+
+    # グレースケールに変換
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 平滑化 (ガウシアンフィルタを適応)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    # エッジを検出 (Canny法)
+    edges = cv2.Canny(blurred, 50, 150)
+
+    # エッジを輪郭として抽出
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # エッジが大きい順にソート
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    for i, contour in enumerate(contours):
+        # contourを囲む最小の矩形を取得する
+        x, y, w, h = cv2.boundingRect(contour)
+        # 切り取る領域を設定する
+        region = img[y:y+h, x:x+w]
+        # 領域を切り取る
+        cv2.imwrite("output/{}.png".format(i), region)
 
     return "rgb(0, 0, 0)"
