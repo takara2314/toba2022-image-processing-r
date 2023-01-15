fb4dc8586060482b5f3e42107b40c74b6549788b
takara2314 <takara2314_TK@outlook.jp>
2023-01-16 07:47:13 +0900

    Complete: 必要な処理を書き終えた

--- kabegami_system/suggest.py
+++ kabegami_system/suggest.py
@@ -1,40 +1,112 @@
-from statistics import mode
 import cv2
-from kabegami_system.load_img import load_img
-from kabegami_system.get_img_color import get_color
+import numpy as np
 
-# 壁紙の色を提案する
-# コードを貼るならこの部分
-def suggest(img_path):
-    # cv2.imread(img_path) と同様
-    img = load_img(img_path)
+# HSVの色相と彩度を指定
+theme_color = {
+    "warm": [
+        [170, 150],
+        [180, 160]
+    ],
+    "minimalist": [
+        [170, 150],
+        [180, 160]
+    ],
+    "cool": [
+        [170, 150],
+        [180, 160]
+    ]
+}
 
-    # グレースケールに変換
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # 平滑化 (ガウシアンフィルタを適応)
-    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+def suggest(img_path, theme):
+    print("Start processing...")
 
-    # エッジを検出 (Canny法)
-    edges = cv2.Canny(blurred, 50, 150)
+    # 画像の読み込み
+    img = cv2.imread(img_path)
+    # HSVに変換
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    original_hsv = img_hsv.copy()
 
-    # エッジを輪郭として抽出
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # 一番多い色相を取得 (色相ヒストグラムを生成)
+    h, _, _ = cv2.split(img_hsv)
+    h_raveled = h.ravel()
+    h_hist = get_histogram_array(h_raveled, 180)
 
-    # エッジが大きい順にソート
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    colors = [None] * len(contours)
+    # 一番多い色相に近い色相をマスク
+    h_max = np.argmax(h_hist)
 
-    for i, contour in enumerate(contours):
-        # contourを囲む最小の矩形を取得する
-        x, y, w, h = cv2.boundingRect(contour)
-        # 切り取る領域を設定する
-        region = img[y:y+h, x:x+w]
-        colors[i] = get_color(region)
-        # 領域を切り取る
-        cv2.imwrite("output/{}.png".format(i), region)
+    hsv_min = np.array([
+        h_max - 30 if h_max - 30 >= 0 else 0,
+        0,
+        0
+    ])
 
-    # 一番多い要素を抽出
-    print(mode(colors))
+    hsv_max = np.array([
+        h_max + 30 if h_max + 30 <= 180 else 0,
+        255,
+        255
+    ])
 
-    return "rgb(0, 0, 0)"
+    masked = cv2.inRange(img_hsv, hsv_min, hsv_max)
+
+    # マスクされなかったところを黒く塗りつぶす
+    img_masked = img.copy()
+    img_masked[masked == 0] = [0, 0, 0]
+
+    # マスクされた部分から一番多い彩度を取得 (0(黒)の場合は2番目に多い彩度)
+    img_hsv = cv2.cvtColor(img_masked, cv2.COLOR_BGR2HSV)
+    _, s, _ = cv2.split(img_hsv)
+    s_raveled = s.ravel()
+    s_hist = get_histogram_array(s_raveled, 256)
+
+    s_hist_sorted = s_hist.argsort()[::-1]
+
+    s_max = 0
+    if s_hist_sorted[0] == 0:
+        s_max = s_hist_sorted[1]
+    else:
+        s_max = s_hist_sorted[0]
+
+    # 元画像の色相と、マスクされた部分の中で一番多い彩度から近い色をマスク
+    hsv_min = np.array([
+        h_max - 30 if h_max - 30 >= 0 else 0,
+        s_max - 30 if s_max - 30 >= 0 else 0,
+        0
+    ])
+
+    hsv_max = np.array([
+        h_max + 30 if h_max + 30 <= 180 else 0,
+        s_max + 30 if s_max + 30 <= 255 else 0,
+        255
+    ])
+
+    masked = cv2.inRange(img_hsv, hsv_min, hsv_max)
+
+    print("Wallpaper detected.")
+
+    # マスクした領域を指定したテーマの色に変換し、画像に反映させる
+    results = [None] * len(theme_color[theme])
+
+    for i, hsv in enumerate(theme_color[theme]):
+        print(f"{theme} theme: {i + 1} / {len(theme_color[theme])}")
+
+        retouched = original_hsv.copy()
+        for y, row in enumerate(masked):
+            for x, col in enumerate(row):
+                if col == 255:
+                    retouched[y][x][0:2] = hsv
+        results[i] = cv2.cvtColor(retouched, cv2.COLOR_HSV2BGR)
+
+        print("done.")
+
+    print("Finished processing.")
+    return results
+
+
+def get_histogram_array(arr, max_num):
+    hist = np.zeros(max_num + 1, dtype=np.int32)
+
+    for i in range(max_num + 1):
+        hist[i] = arr[arr == i].shape[0]
+
+    return hist
