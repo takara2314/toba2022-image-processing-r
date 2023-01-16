4dd643809731f59899341588ccdf01ee2ae0368e
takara2314 <takara2314_TK@outlook.jp>
2023-01-16 09:04:57 +0900

    Improve: cool theme

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import kabegami_system
 
 # 入力（引数に検出させたい画像のパスを入れる）
-outputs = kabegami_system.suggest("resources/room (0).png", "cool")
+outputs = kabegami_system.suggest("resources/room (8).png", "cool")
 
 # 出力
 for i, img in enumerate(outputs):
